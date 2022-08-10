722c4f7dc7dccb34d95e1a12d73979db8a06643f
sksmitshoek <s.k.smitshoek@gmail.com>
2022-08-10 10:34:07 -0600

    initial_upload

---
+++ Exercises/Exercise 2.1/.ipynb_checkpoints/exercise_2_ssmitshoek-checkpoint.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('Hello Word!  I wonder why that is always the default coding text to start with')
+
+
+# In[2]:
+
+
+add = 6 + 3
+print(add)
+
+
+# In[3]:
+
+
+sub = 6 - 3
+print(sub)
+
+
+# In[4]:
+
+
+mul = 6 * 3
+print(mul)
+
+
+# In[5]:
+
+
+div = 6 / 3
+print(div)
+
+
+# In[6]:
+
+
+str_con = 'Hello' + ' ' +'world!'
+print(str_con)
+
+
+# In[7]:
+
+
+fruit = ['banana', 'apple', 'orange', 'lemon']
+print(fruit)
+
+
+# In[8]:
+
+
+fruit.append('blueberries')
+print(fruit)
+
+
+# In[9]:
+
+
+vegetable = ('carrot', 'broccoli', 'celery', 'onion')
+print(vegetable)
+
+
+# In[ ]:
+
+
+
+
